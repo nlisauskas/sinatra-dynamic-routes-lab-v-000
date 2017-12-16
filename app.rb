01988f1284e95431943daed7107a35972aedcd63
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-16 20:52:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,24 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get 'reversename/:name' do
-    
+  get '/reversename/:name' do
+
   end
 
-  get 'square/:number' do
-    
+  get '/square/:number' do
+
   end
 
-  get 'say/:number/:phrase' do
-    
+  get '/say/:number/:phrase' do
+
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
-    
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
   end
 
   get '/:operation/:number1/:number2' do
-    
+
   end
 
 end
