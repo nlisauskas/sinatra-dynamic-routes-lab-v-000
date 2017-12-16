9e961f8a71235c4aa2f27bc767f87591d0eb26ad
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-16 21:12:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,6 +32,15 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if @operation == "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation == "add"
+        "#{@number1 + @number2}"
+    elsif @operation == "multiply"
+        "#{@number1 * @number2}"
+    elsif @operation == "divide"
+        "#{@number1 / @number2}"
+    end
+
 
   end
 
