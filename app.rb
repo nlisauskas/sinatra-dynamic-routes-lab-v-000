36fc919db93d61388f30d97da6b0cba5055c1ad3
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-16 20:58:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    @number.times do "#{@phrase}"
+    @number.times do "#{@phrase}" end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -28,7 +28,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-
+    
   end
 
 end
